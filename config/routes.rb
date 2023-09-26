6662b15af1686630b0b21665f3216a72b074189a
Jerome <fabricantejerome@gmail.com>
2023-09-26 11:07:36 +0800

    Allow user to register and store session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Get /about
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
+  delete "logout", to: "sessions#destroy"
   
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
